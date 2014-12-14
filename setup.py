24d1e916a28f7538d6dd9dd68f552a35b31b3f02
James Myatt <james@jamesmyatt.co.uk>
2014-12-14 14:16:47 +0000

    Initial project structure

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+config = {
+    'description': 'SensorMonitor',
+    'author': 'James Myatt',
+    'url': 'https://github.com/Nzbuu/SensorMonitor',
+    'version': '0.0.1',
+    'install_requires': [],
+    'packages': ['SensorMonitor'],
+    'name': 'SensorMonitor'
+}
+
+setup(**config)
---
+++ tests/SensorMonitor_tests.py
@@ -0,0 +1,11 @@
+import unittest
+import SensorMonitor
+
+
+class TestSensorMonitor(unittest.TestCase):
+    def test_nothing(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
