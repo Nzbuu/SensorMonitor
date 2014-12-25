a6e444a42181252e4990994af21a9e4e5d0f3aab
James Myatt <james@jamesmyatt.co.uk>
2014-12-25 12:19:05 +0000

    Expand stub monitor class

--- SensorMonitor/Monitor.py
+++ SensorMonitor/Monitor.py
@@ -6,10 +6,21 @@ __author__ = 'James Myatt'
 class Monitor(object):
     def __init__(self):
         self.name = "Sensor Monitor"
-        self._sensors = []
-        self._triggers = []
-        self._loggers = []
+        self.__sensors = []
+        self.__triggers = []
+        self.__loggers = []
+    
+    def __del__(self):
         pass
+
+    def add_sensor(self, sensor):
+        self.__sensors.append(sensor)
+    
+    def add_trigger(self, trigger):
+        self.__triggers.append(trigger)
+        
+    def add_logger(self, logger):
+        self.__loggers.append(logger)
 
     def run(self):
         print(self.name)
