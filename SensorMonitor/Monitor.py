069dca4ecfcae662670abbdc4a2153fac4eb9673
James Myatt <james@jamesmyatt.co.uk>
2015-01-21 22:03:34 +0000

    (Monitor) Add loops over sensors and loggers. Record limited number of measurements before stopping automatically.

--- SensorMonitor/Monitor.py
+++ SensorMonitor/Monitor.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import time
 
 __author__ = 'James Myatt'
@@ -9,20 +10,38 @@ class Monitor(object):
         self.__sensors = []
         self.__triggers = []
         self.__loggers = []
-    
+        self.time_step = 1
+        self.num_stop = 5
+        self.__num_meas = 0
+
     def __del__(self):
         pass
 
     def add_sensor(self, sensor):
         self.__sensors.append(sensor)
-    
+
     def add_trigger(self, trigger):
         self.__triggers.append(trigger)
-        
+
     def add_logger(self, logger):
         self.__loggers.append(logger)
 
     def run(self):
         print(self.name)
-        while True:
-            time.sleep(10)
+        while self.__num_meas < self.num_stop:
+            result = self.get_measurements()
+            self.store_measurements(result)
+            self.__num_meas += 1
+            time.sleep(self.time_step)
+
+    def get_measurements(self):
+        result = {'timestamp': datetime.now()}
+        for sensor in self.__sensors:
+            sensor_data = sensor.get_measurement()
+            result[sensor.identifier] = sensor_data
+        return result
+
+    def store_measurements(self, result):
+        print("Sample", result['timestamp'])
+        for logger in self.__loggers:
+            logger.store_measurement(result)
