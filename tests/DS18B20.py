f073448b805190373101f70967d7732c98808f29
James Myatt <james@jamesmyatt.co.uk>
2015-07-22 22:13:06 +0100

    (tests) Additional test cases

--- tests/DS18B20.py
+++ tests/DS18B20.py
@@ -24,6 +24,36 @@ class TestDS18B20(unittest.TestCase):
         obj.device_file = 'DS18B20_TEST'
         self.assertEqual(obj.read_sensor(), 27.625)
 
+    @patch('__builtin__.open', mock_open(
+        read_data='ba 01 55 00 7f ff 0c 10 0a : crc=0a NO\nba 01 55 00 7f ff 0c 10 0a t=27625\n'))
+    def test_NOK_detailed_file(self):
+        obj = DS18B20()
+        obj.device_file = 'DS18B20_TEST'
+        self.assertEqual(obj.read_sensor(), None)
+
+    @patch('__builtin__.open', mock_open(
+        read_data='ba 01 55 00 7f ff 0c 10 0a : crc=0a YES\n'
+                  'ba 01 55 00 7f ff 0c 10 0a t=27625\n'
+                  'ba 01 55 00 7f ff 0c 10 0a t=30000\n'
+                  'ba 01 55 00 7f ff 0c 10 0a t=32000\n'))
+    def test_extended_file_ignores_extra_lines(self):
+        obj = DS18B20()
+        obj.device_file = 'DS18B20_TEST'
+        self.assertEqual(obj.read_sensor(), 27.625)
+
+    @patch('__builtin__.open', mock_open(
+        read_data='ba 01 55 00 7f ff 0c 10 0a : crc=0a NO\n'))
+    def test_truncated_file(self):
+        obj = DS18B20()
+        obj.device_file = 'DS18B20_TEST'
+        self.assertEqual(obj.read_sensor(), None)
+
+    @patch('__builtin__.open', mock_open(
+        read_data='ba 01 55 00 7f ff 0c 10 0a : crc=0a YES\n'))
+    def test_truncated_file_is_NOK(self):
+        obj = DS18B20()
+        obj.device_file = 'DS18B20_TEST'
+        self.assertEqual(obj.read_sensor(), None)
 
 if __name__ == '__main__':
     unittest.main()
