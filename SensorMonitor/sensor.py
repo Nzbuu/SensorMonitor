27ee30471958d81264810051468260a9e0a3ecac
James Myatt <james@jamesmyatt.co.uk>
2015-07-24 00:23:52 +0100

    (Therm) Rename to W1Therm. Extract factory class and use FileAccessWrapper class. Update tests accordingly.

--- SensorMonitor/sensor.py
+++ SensorMonitor/sensor.py
@@ -2,31 +2,40 @@ __author__ = 'James'
 
 import os
 
-
 class Sensor:
     def __init__(self):
         pass
 
 
-class DS18B20(Sensor):
-    def __init__(self, sensor_id=''):
-        Sensor.__init__(self)
-        self.sensorID = sensor_id
+class FileAccessWrapper:
+    def __init__(self, filename):
+        self.filename = filename
 
+    def open(self):
+        return open(self.filename, 'r')
+
+
+class W1ThermFactory:
+    def __init__(self):
         self.base_dir = '/sys/bus/w1/devices'
-        self.device_file = ''
 
-    def init_sensor(self):
-        if not self.device_file:
-            if not self.sensorID:
-                device_folder = [x for x in os.listdir(self.base_dir) if x.startswith('28')]
-                self.sensorID = device_folder[0]
-            self.device_file = self.base_dir + '/' + self.sensorID + '/w1_slave'
+    def create(self, sensor_id=None):
+        if not sensor_id:
+            device_folder = [x for x in os.listdir(self.base_dir) if x.startswith('28')]
+            sensor_id = device_folder[0]
+
+        device_file = self.base_dir + '/' + sensor_id + '/w1_slave'
+        return W1Therm(sensor_id=sensor_id, file_access=FileAccessWrapper(device_file))
+
+
+class W1Therm(Sensor):
+    def __init__(self, sensor_id='', file_access=None):
+        Sensor.__init__(self)
+        self.sensor_id = sensor_id
+        self.file_access = file_access
 
     def read_sensor(self):
-        self.init_sensor()
-
-        with open(self.device_file, 'rb') as f:
+        with self.file_access.open() as f:
             line = f.readline().strip()
             if not line or not line.endswith('YES'):
                 return None
