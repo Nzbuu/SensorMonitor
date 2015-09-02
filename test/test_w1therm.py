6c0de6834fa91e8bc2031074ba1f33eefab3e291
James Myatt <james@jamesmyatt.co.uk>
2015-09-02 13:45:05 +0100

    (Test) Replace FakeAccessWrapper with mock object

--- test/test_w1therm.py
+++ test/test_w1therm.py
@@ -1,4 +1,5 @@
 import unittest
+from mock import Mock
 import io
 
 from SensorMonitor.w1therm import *
@@ -7,74 +8,82 @@ from SensorMonitor.w1therm import W1Therm
 
 class W1ThermFileTests(unittest.TestCase):
     def test_minimal_OK(self):
-        text = u'YES\nt=20000\n'
-        obj = interface.FileInterface(
-            file_access=FakeAccessWrapper(text))
+        mock_file = Mock(interface.FileAccessWrapper)
+        mock_file.open.return_value = io.StringIO(
+            u'YES\nt=20000\n')
+
+        obj = interface.FileInterface(file_access=mock_file)
         sens = W1Therm(obj)
         self.assertEqual(sens.get_measurement(), 20.000)
 
     def test_minimal_NOK(self):
-        text = u'NO\nt=20000\n'
-        obj = interface.FileInterface(
-            file_access=FakeAccessWrapper(text))
+        mock_file = Mock(interface.FileAccessWrapper)
+        mock_file.open.return_value = io.StringIO(
+            u'NO\nt=20000\n')
+
+        obj = interface.FileInterface(file_access=mock_file)
         sens = W1Therm(obj)
         self.assertEqual(sens.get_measurement(), None)
 
     def test_OK_detailed_file(self):
-        text = u'ba 01 55 00 7f ff 0c 10 0a : crc=0a YES\n' \
-               u'ba 01 55 00 7f ff 0c 10 0a t=27625\n'
-        obj = interface.FileInterface(
-            file_access=FakeAccessWrapper(text))
+        mock_file = Mock(interface.FileAccessWrapper)
+        mock_file.open.return_value = io.StringIO(
+            u'ba 01 55 00 7f ff 0c 10 0a : crc=0a YES\n'
+            u'ba 01 55 00 7f ff 0c 10 0a t=27625\n')
+
+        obj = interface.FileInterface(file_access=mock_file)
         sens = W1Therm(obj)
         self.assertEqual(sens.get_measurement(), 27.625)
 
     def test_NOK_detailed_file(self):
-        text = u'ba 01 55 00 7f ff 0c 10 aa : crc=0a NO\n' \
-               u'ba 01 55 00 7f ff 0c 10 aa t=27625\n'
-        obj = interface.FileInterface(
-            file_access=FakeAccessWrapper(text))
+        mock_file = Mock(interface.FileAccessWrapper)
+        mock_file.open.return_value = io.StringIO(
+            u'ba 01 55 00 7f ff 0c 10 aa : crc=0a NO\n'
+            u'ba 01 55 00 7f ff 0c 10 aa t=27625\n')
+
+        obj = interface.FileInterface(file_access=mock_file)
         sens = W1Therm(obj)
         self.assertEqual(sens.get_measurement(), None)
 
     def test_extended_file_ignores_extra_lines(self):
-        text = u'ba 01 55 00 7f ff 0c 10 0a : crc=0a YES\n' \
-               u'ba 01 55 00 7f ff 0c 10 0a t=27625\n' \
-               u'ba 01 55 00 7f ff 0c 10 0a t=30000\n' \
-               u'ba 01 55 00 7f ff 0c 10 0a t=32000\n'
-        obj = interface.FileInterface(
-            file_access=FakeAccessWrapper(text))
+        mock_file = Mock(interface.FileAccessWrapper)
+        mock_file.open.return_value = io.StringIO(
+            u'ba 01 55 00 7f ff 0c 10 0a : crc=0a YES\n'
+            u'ba 01 55 00 7f ff 0c 10 0a t=27625\n'
+            u'ba 01 55 00 7f ff 0c 10 0a t=30000\n'
+            u'ba 01 55 00 7f ff 0c 10 0a t=32000\n')
+
+        obj = interface.FileInterface(file_access=mock_file)
         sens = W1Therm(obj)
         self.assertEqual(sens.get_measurement(), 27.625)
 
     def test_truncated_file(self):
-        text = u'ba 01 55 00 7f ff 0c 10 aa : crc=0a NO\n'
-        obj = interface.FileInterface(
-            file_access=FakeAccessWrapper(text))
+        mock_file = Mock(interface.FileAccessWrapper)
+        mock_file.open.return_value = io.StringIO(
+            u'ba 01 55 00 7f ff 0c 10 aa : crc=0a NO\n')
+
+        obj = interface.FileInterface(file_access=mock_file)
         sens = W1Therm(obj)
         self.assertEqual(sens.get_measurement(), None)
 
     def test_truncated_file_is_NOK(self):
-        text = u'ba 01 55 00 7f ff 0c 10 0a : crc=0a YES\n'
-        obj = interface.FileInterface(
-            file_access=FakeAccessWrapper(text))
+        mock_file = Mock(interface.FileAccessWrapper)
+        mock_file.open.return_value = io.StringIO(
+            u'ba 01 55 00 7f ff 0c 10 0a : crc=0a YES\n')
+
+        obj = interface.FileInterface(file_access=mock_file)
         sens = W1Therm(obj)
         self.assertEqual(sens.get_measurement(), None)
 
     def test_missing_temperature(self):
-        text = u'ba 01 55 00 7f ff 0c 10 0a : crc=0a YES\n' \
-               u'ba 01 55 00 7f ff 0c 10 0a\n'
-        obj = interface.FileInterface(
-            file_access=FakeAccessWrapper(text))
+        mock_file = Mock(interface.FileAccessWrapper)
+        mock_file.open.return_value = io.StringIO(
+            u'ba 01 55 00 7f ff 0c 10 0a : crc=0a YES\n'
+            u'ba 01 55 00 7f ff 0c 10 0a\n')
+
+        obj = interface.FileInterface(file_access=mock_file)
         sens = W1Therm(obj)
         self.assertEqual(sens.get_measurement(), None)
-
-
-class FakeAccessWrapper:
-    def __init__(self, text):
-        self.text = text
-
-    def open(self):
-        return io.StringIO(self.text)
 
 
 if __name__ == '__main__':
